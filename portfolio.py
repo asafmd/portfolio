4302d9bfec76467686b8db04a93915d435539329
Asaf Mohammed <mohamedmd2500@gmail.com>
2025-04-19 17:13:33 +0530

    Uploading portfolio code

---
+++ portfolio.py
@@ -0,0 +1,73 @@
+import streamlit as st
+
+
+
+st.title("Asaf Mohammed")
+st.markdown('##### Data Analyst/Engineer | Python | SQL | Tableau | Power BI | Excel')
+st.markdown('##### Chennai, India')
+st.markdown('##### asafmohammed21@gmail.com | www.linkedin.com/in/asaf-mohammed')
+
+st.subheader("About Me")
+st.write(
+    """ 
+    Analyst with 4+ years of experience including SQL, data analysis, and automation, skilled in driving decision-making through focused 
+    and conclusive insights. Proficient in designing and implementing analytical projects that align with business goals, 
+    with a solid track record in building dashboards and reports for strategic decision-making. Hands-on experience with SQL, Python, Excel, 
+    and data visualization tools. Adept at collaborating with cross-functional teams, including Business, Product, and 
+    Data Engineering, to improve data accuracy, define metrics, and drive process improvements.
+    Passionate about leveraging data to solve complex business problems and drive growth.
+    """
+)
+
+st.subheader("Skills")
+st.write(
+    """
+    - **Programming Languages:** Python, SQL, PySpark (basic)
+    - **Data Analysis Libraries:** Pandas, NumPy, Matplotlib
+    - **Data Visualization Tools:** Tableau, Power BI
+    - **Databases:** Snowflake, SQL Server, MySQL, PostgreSQL
+    - **Other Tools:** Excel, Jupyter Notebook
+    """
+)
+
+st.subheader("Experience")
+st.markdown('#### Data Services Analyst') 
+st.markdown('#### ZoomInfo  (Jun 2023 - Present)') 
+st.write(
+    """
+    •	Built an email validation automation system using Python and PySpark, reducing manual work by 200 cases per quarter and saving 10 man-days per quarter.\n
+    •	Built Utils script for Salesforce for our internal scripts thereby reducing the time and effort spent for rewriting the code by around 20%.\n
+    •	Created SQL queries in Snowflake for data manipulation, supporting use cases such as improving customer satisfaction.\n
+    •	Collaborated with Product and Data Engineering teams to ensure data accuracy and completeness in the GCP Full File Collector script, enhancing the overall data quality.\n
+    •	Worked on creating Tableau Reports for customers as per their requirements.\n
+
+    """
+)
+
+
+st.markdown('#### Senior Research Analyst / Research Analyst') 
+st.markdown('#### Altrata (formerly Relationship Science) - (2020 - 2023)') 
+st.write(
+    """
+•	Developed SQL-based solutions for complex data analysis and participated in a large-scale data migration from SQL Server to Databricks, ensuring seamless data flow and accuracy.\n
+•	Worked closely with Business and Data teams to define critical business metrics and build frameworks for measuring the performance of various business units.\n
+•	Supported business process improvements through data-driven insights and analysis, enhancing decision-making for senior management.\n
+
+    """
+)
+
+st.subheader("Key Projects")
+st.markdown('#### Email Validation Automation')
+st.write("Developed a Python automation script for email validation, reducing manual workload by 200 cases per quarter and saving 10 man-days, improving data accuracy.")
+
+st.markdown("#### Salesforce Utils Script")
+st.write("Created a Utils script for Salesforce, reducing code rewriting time and effort by 20%, enhancing efficiency in data processing.")
+
+st.markdown('#### GCP Full File Collector Script')
+st.write('Delivered a Python script for GCP file collection ahead of time, working closely with engineering teams to ensure complete and accurate data collection.')
+
+st.subheader("Education")
+st.markdown('##### Bachelor of Technology in Mechanical Engineering')
+st.markdown('##### Aalim Muhammed Salegh College of Engineering, Chennai (2015 - 2019)')
+
+
